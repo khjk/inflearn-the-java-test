95a4165b85ae624ebfabd4ac904adc2a66789bc6
khjk <begywjd@naver.com>
2020-08-13 11:00:00 +0900

    SimpleArgumentConverter, ArgumentsAggregator 만들어서 단일 또는 다중 argument 데이터를 클래스로 변환해서 테스트하기

--- src/main/java/me/khjk/Study.java
+++ src/main/java/me/khjk/Study.java
@@ -5,12 +5,20 @@ public class Study {
 
     private int limit;
 
+    private String name;
+
     public Study() {
     }
+
     public Study(int limit) {
         if(limit < 0)
             throw new IllegalArgumentException("limit은 0보다 커야 한다.");
         this.limit = limit;
+    }
+
+    public Study(int limit, String name) {
+        this.limit = limit;
+        this.name = name;
     }
 
     public StudyStatus getStatus() {
@@ -19,5 +27,22 @@ public class Study {
 
     public int getLimit() {
         return limit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Study{" +
+                "status=" + status +
+                ", limit=" + limit +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
--- src/test/java/me/khjk/RepeatTest.java
+++ src/test/java/me/khjk/RepeatTest.java
@@ -1,12 +1,20 @@
 package me.khjk;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.*;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Convert;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+
 class RepeatTest {
 
     @DisplayName("반복 테스트 만들기")
@@ -18,7 +26,63 @@ class RepeatTest {
     @DisplayName("다른 값들 인자로 받아서 테스트 만들기")
     @ParameterizedTest(name = "{index} {displayName} message={0}") //테스트마다 다른 값들을 가지고 테스트 하고싶다..
     @ValueSource(strings = {"날씨가", "많이", "더워요","!!"})
+    @NullAndEmptySource //null 과 empty 한 값 두개를 파라미터로 추가 테스트
     void repeat_other_value_Test(String message) {
         System.out.println(message);
     }
+
+    @DisplayName("다른 값들 인자로 받아서 테스트 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message={0}") //테스트마다 다른 값들을 가지고 테스트 하고싶다..
+    @ValueSource(ints = {10, 20, 30})
+    void repeat_test_using_converter(@ConvertWith(StudyConverter.class) Study study) {
+        System.out.println(study.getLimit());
+    }
+
+    /**
+     * 자동형변환 Converter
+     * ArgumentConverter은 한개의 argument에 대한 것...
+     * */
+    static class StudyConverter extends SimpleArgumentConverter {
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            assertEquals(Study.class, targetType, "Can only convert to Study");
+            return new Study(Integer.parseInt(source.toString()));
+        }
+    }
+
+    ////////////////////////////////////////////////////////////
+
+    @DisplayName("csvSource 사용 해서 반복 테스트 수행하기")
+    @ParameterizedTest(name = "{index} {displayName} message={0}") //테스트마다 다른 값들을 가지고 테스트 하고싶다..
+    @CsvSource({"10, '자바 스터디'", "20, '스프링'"})
+    void repeat_test_using_csvSource(Integer limit, String name) {
+        Study study = new Study(limit, name);
+        System.out.println(study);
+    }
+
+    @DisplayName("argumentsAccessor 사용 해서 반복 테스트 수행하기")
+    @ParameterizedTest(name = "{index} {displayName} message={0}") //테스트마다 다른 값들을 가지고 테스트 하고싶다..
+    @CsvSource({"10, '자바 스터디'", "20, '스프링'"})
+    void repeat_test_using_csvSource2(ArgumentsAccessor argumentsAccessor) {
+        Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+        System.out.println(study);
+    }
+
+    @DisplayName("ArgumentsAggregator 만들어서 해서 반복 테스트 수행하기")
+    @ParameterizedTest(name = "{index} {displayName} message={0}") //테스트마다 다른 값들을 가지고 테스트 하고싶다..
+    @CsvSource({"10, '자바 스터디'", "20, '스프링'"})
+    void repeat_test_using_csvSource3(@AggregateWith(StudyAggregator.class) Study study) {
+        System.out.println(study);
+    }
+    /**
+     * argument가 여러개면 ArgumentsAggregator을 생성
+     * */
+    static class StudyAggregator implements ArgumentsAggregator {
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext parameterContext) throws ArgumentsAggregationException {
+            Study study = new Study(accessor.getInteger(0), accessor.getString(1));
+            return study;
+        }
+    }
+
 }
