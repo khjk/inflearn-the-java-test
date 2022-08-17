0b9459a2881e9c4b4f0552a79b2bffe8a4e211f0
khjk <begywjd@naver.com>
2022-08-17 23:11:06 +0900

    mock 객체 Stubbing

--- src/main/java/me/khjk/domain/Member.java
+++ src/main/java/me/khjk/domain/Member.java
@@ -1,4 +1,22 @@
 package me.khjk.domain;
 
 public class Member {
+    public long Id;
+    public String Email;
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
 }
--- src/main/java/me/khjk/domain/Study.java
+++ src/main/java/me/khjk/domain/Study.java
@@ -2,6 +2,29 @@ package me.khjk.domain;
 
 public class Study {
     public Member owner;
+    public int limit;
+    public String name;
+
+    public Study(int limit, String name) {
+        this.limit = limit;
+        this.name = name;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public Member getOwner() {
         return owner;
